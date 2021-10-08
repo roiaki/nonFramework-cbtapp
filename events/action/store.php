c86319e5ba32fd4eeabea4afe5924b732d156451
roiaki <roitoitoitoi@yahoo.co.jp>
2021-10-08 16:42:52 +0900

    events/store.phpバリデーション変更

--- events/action/store.php
+++ events/action/store.php
@@ -1,97 +1,99 @@
 <?php
-    require '../../common/database.php';
-    require '../../common/auth.php';
-    require '../../common/validation.php';
+session_start();
 
-    if (!isLogin()) {
-        header('Location: ../../login/');
-    }
+require '../../common/database.php';
+require '../../common/auth.php';
+require '../../common/validation.php';
 
-    $htmltitle = "";
-    $clean['title'] = htmlspecialchars( $_POST['title'], ENT_QUOTES, 'UTF-8');
-    $clean['content'] = htmlspecialchars( $_POST['content'], ENT_QUOTES, 'UTF-8');
+if (!isLogin()) {
+    header('Location: ../../login/');
+}
 
-    if (emptyCheck($clean['title']) == "empty") {
-        $_SESSION['title_error'][] = "タイトル入力必須です";
-    }
+$htmltitle = "";
+$clean['title'] = htmlspecialchars( $_POST['title'], ENT_QUOTES, 'UTF-8');
+$clean['content'] = htmlspecialchars( $_POST['content'], ENT_QUOTES, 'UTF-8');
 
-    if (emptyCheck($clean['title']) == "empty") {
-        $_SESSION['title_error'][] = "タイトル入力必須です";
-    }
+if (emptyCheck($clean['title']) == "NG") {
+    $_SESSION['error_title']['empty'] = "タイトル入力必須です";
+}
 
-     // バリデーションエラーがあったら同じ画面へ
-     if ($_SESSION['email_error']) {
-        //$post = 
-        header('Location: ../../register');
-        exit;
-    }
-    /*
-    if ( empty($_POST['title']) ) {
-        $_SESSION['error_message'] = "タイトルを入力してください";
-        var_dump($_POST);
-        var_dump($_SESSION);
-        //exit;
-        header('Location: ../../events/create.php');
-        exit;
-    } else {
-        $clean['title'] = htmlspecialchars( $_POST['title'], ENT_QUOTES, 'UTF-8' );
-        header('Location: ../../events/create.php');
-    }
+if (emptyCheck($clean['content']) == "NG") {
+    $_SESSION['error_content']['empty'] = "内容入力必須です";
+}
+
+    // バリデーションエラーがあったら同じ画面へ
+    if ($_SESSION['error_title'] || $_SESSION['error_content']) {
+    //$post = 
+    header('Location: ../../events/create.php');
+    exit;
+}
+/*
+if ( empty($_POST['title']) ) {
+    $_SESSION['error_message'] = "タイトルを入力してください";
+    var_dump($_POST);
+    var_dump($_SESSION);
+    //exit;
+    header('Location: ../../events/create.php');
+    exit;
+} else {
+    $clean['title'] = htmlspecialchars( $_POST['title'], ENT_QUOTES, 'UTF-8' );
+    header('Location: ../../events/create.php');
+}
 */
 /*
-    if ( empty($_POST['content']) ) {
-        $_SESSION['error_message'] = "内容を入力してください";
-        header('Location: ../../events/create.php');
-        exit;
-
-    } else {
-        $clean['content'] = htmlspecialchars( $_POST['content'], ENT_QUOTES, 'UTF-8');
-    }
-  */  
-    $content = $_POST['content'];
-    $created_at = date("Y-m-d H:i:s");
-    $updated_at = date("Y-m-d H:i:s");
-    var_dump($created_at);
-    var_dump($_POST); 
-    //exit;
-
-    $user_id = getLoginUserId();
-    $user_name = getLoginUserName();
-
-
-    $database_handler = getDatabaseConnection();
-    
-    // トランザクション開始
-    $database_handler->beginTransaction();
-
-    try {
-        $sql = $database_handler->prepare("INSERT INTO events (user_id, title, content, updated_at, created_at) VALUES(:user_id, :title, :content, :created_at, :updated_at)");
-        $sql->bindParam(":user_id", $user_id);
-        $sql->bindParam(":title", $clean['title']);
-        $sql->bindParam(":content", $clean['content']);
-        $sql->bindParam(":created_at", $created_at);
-        $sql->bindParam(":updated_at", $created_at);
-        $sql->execute();
-
-        // コミット
-        $res = $database_handler->commit();
-
-    } catch(Exception $e) {
-        // エラーが起きたらロールバック
-        $database_handler->rollBack();
-    }
-
-    if ( $res ) {
-        $succes_message = '保存成功';
-    } else {
-        $error_message['database'] = '保存に失敗しました';
-    }
-    //var_dump($res);
-    //var_dump($succes_message);
-    //exit;
-    $sql = null;
-
-    $database_handler = null;
-
-    header('Location: ../../events');
+if ( empty($_POST['content']) ) {
+    $_SESSION['error_message'] = "内容を入力してください";
+    header('Location: ../../events/create.php');
     exit;
+
+} else {
+    $clean['content'] = htmlspecialchars( $_POST['content'], ENT_QUOTES, 'UTF-8');
+}
+*/  
+$content = $_POST['content'];
+$created_at = date("Y-m-d H:i:s");
+$updated_at = date("Y-m-d H:i:s");
+var_dump($created_at);
+var_dump($_POST); 
+//exit;
+
+$user_id = getLoginUserId();
+$user_name = getLoginUserName();
+
+
+$database_handler = getDatabaseConnection();
+
+// トランザクション開始
+$database_handler->beginTransaction();
+
+try {
+    $sql = $database_handler->prepare("INSERT INTO events (user_id, title, content, updated_at, created_at) VALUES(:user_id, :title, :content, :created_at, :updated_at)");
+    $sql->bindParam(":user_id", $user_id);
+    $sql->bindParam(":title", $clean['title']);
+    $sql->bindParam(":content", $clean['content']);
+    $sql->bindParam(":created_at", $created_at);
+    $sql->bindParam(":updated_at", $created_at);
+    $sql->execute();
+
+    // コミット
+    $res = $database_handler->commit();
+
+} catch(Exception $e) {
+    // エラーが起きたらロールバック
+    $database_handler->rollBack();
+}
+
+if ( $res ) {
+    $succes_message = '保存成功';
+} else {
+    $error_message['database'] = '保存に失敗しました';
+}
+//var_dump($res);
+//var_dump($succes_message);
+//exit;
+$sql = null;
+
+$database_handler = null;
+
+header('Location: ../../events');
+exit;
--- events/create.php
+++ events/create.php
@@ -1,6 +1,10 @@
 <?php
+session_start();
+
 require '../common/database.php';
 require '../common/auth.php';
+//var_dump($_SESSION);
+//exit();
 
 if (!isLogin()) {
 	header('Location: ../../login/');
@@ -23,28 +27,38 @@ include('../common/head.php');
 
 		<div class="col-5">
 			<h3>出来事　新規作成</h3>
-			<?php if (isset($_SESSION['error_message'])) {
-				echo '<div class="text-danger">';
-				var_dump($_SESSION);
-				foreach ($_SESSION['error_message'] as $error) {
-					echo "<div>* $error </div>";
-				}
-				echo '</div>';
-				unset($_SESSION['error_message']);
-			}
-			?>
+			
 			<form method="post" action="action/store.php">
 				<div class="form-group">
 					<label>出来事タイトル</label>
 					<input type="text" class="form-control" id="title" name="title">
-
 				</div>
+				<?php if (isset($_SESSION['error_title'])) {
+				echo '<div class="text-danger">';
+				//var_dump($_SESSION);
+				foreach ($_SESSION['error_title'] as $error) {
+					echo "<div>* $error </div>";
+				}
+				echo '</div>';
+				unset($_SESSION['error_title']);
+				}
+				?>
 
 				<div class="form-group">
 					<!-- 内容 -->
 					<label for="content">出来事 の 内容</label>
 					<textarea class="form-control" id="content" name="content" cols="90" rows="7"></textarea>
 				</div>
+				<?php if (isset($_SESSION['error_content'])) {
+				echo '<div class="text-danger">';
+				//var_dump($_SESSION);
+				foreach ($_SESSION['error_content'] as $error) {
+					echo "<div>* $error </div>";
+				}
+				echo '</div>';
+				unset($_SESSION['error_content']);
+				}
+				?>
 				<button class="btn btn-primary" type="submit">作成</button>
 			</form>
 		</div>
