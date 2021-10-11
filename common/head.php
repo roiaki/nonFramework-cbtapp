b6a074fa8c215b5e3728516646de155c7aa6f877
roiaki <roitoitoitoi@yahoo.co.jp>
2021-10-11 21:40:55 +0900

    indent test

--- common/head.php
+++ common/head.php
@@ -3,8 +3,8 @@
 
 <head>
   <meta charset="utf-8">
-	<title><?php echo $htmltitle; ?></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-	<link rel="stylesheet" href="../../public/css/main.css">
+  <title><?php echo $htmltitle; ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../../public/css/main.css">
 </head>
